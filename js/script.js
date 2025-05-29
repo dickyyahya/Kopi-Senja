ff5764a145bcd7bc8a2d95d42f77e3fc74d1c311
savon <savon3481@gmail.com>
2025-05-29 15:23:47 +0700

    tambah modal box

--- js/script.js
+++ js/script.js
@@ -39,3 +39,30 @@ document.querySelector("#search-button").onclick = (e) => {
   searchBox.focus();
   e.preventDefault();
 };
+
+// modal box
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik close modal
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
